eca27a7f91c3540c1aa07861e5122de8f707b171
Rushil-9-11 <shreerushil@gmail.com>
2025-09-09 23:48:52 +0530

    Fix: corrected LinkedIn link in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,26 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="footer container">
-      <div style={{display:"flex", gap:12, alignItems:"center"}}>
-        <div style={{fontWeight:700}}>Rushil Dwaraka</div>
+      <div style={{ display: "flex", gap: 12, alignItems: "center" }}>
+        <div style={{ fontWeight: 700 }}>Rushil Dwaraka</div>
       </div>
 
-      <div style={{display:"flex", gap:16, alignItems:"center"}}>
+      <div style={{ display: "flex", gap: 16, alignItems: "center" }}>
         <div className="socials">
-          
-          <a href="https://github.com/Rushil-9-11" aria-label="github"><FaGithub /></a>
-          
-          <a href="linkedin.com/in/d-b-sai-rushil-640217358/" aria-label="linkedin"><FaLinkedin /></a>
-          
+          <a href="https://github.com/Rushil-9-11" aria-label="github">
+            <FaGithub />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/d-b-sai-rushil-640217358/"
+            aria-label="linkedin"
+          >
+            <FaLinkedin />
+          </a>
         </div>
-        <div style={{color:"var(--muted)", fontSize:18}}> {new Date().getFullYear()}</div>
+        <div style={{ color: "var(--muted)", fontSize: 18 }}>
+          {new Date().getFullYear()}
+        </div>
       </div>
     </footer>
   );
